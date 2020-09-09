f8e50e004c98d1da2ec0d6ad0740fc3da2f4b25b
afolabiopakunle <opakunleafolabi@gmail.com>
2020-09-09 18:26:43 +0100

    app now vuetified

--- js/script.js
+++ js/script.js
@@ -1,18 +1,19 @@
-let box = document.querySelector(".box")
-let submit = document.querySelector("#submit");
-let surround = document.querySelector("#surround")
-submit.addEventListener("click", runIt);
-
-let random = function () {
-    return res = Math.round(Math.random() * 2000)
-}
-
-function runIt(e) {
-    e.preventDefault();
-    surround.style.display = "flex";
-    box.style.display = "block";
-    setTimeout(() => {
-        box.style.display = "none";
-        surround.style.display = "none";
-    }, random());
-}
+new Vue({
+    el: "#root",
+    data: {
+        showCover: false,
+    },
+    methods: {
+        runIt: function (e) {
+            e.preventDefault();
+            this.showCover = true;
+            // box.style.display = "block";
+            setTimeout(() => {
+                this.showCover = false
+            }, this.random());
+        },
+        random: function () {
+            return Math.round(Math.random() * 2000)
+        }
+    }
+})
